09e624c6d7e20f85e6231a89bb7352cb86e216d9
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-19 01:32:52 +0530

    changed colours

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const startServer = async () => {
-    // const res = await axios.get(`https://api.github.com/users/mo-jo-dev/repos?per_page=5`);
-    const res = await axios.get(`https://api.github.com/repos/mo-jo-dev/Github_Repo_Finder/languages`);
+    const res = await axios.get(`https://api.github.com/users/mo-jo-dev/repos?per_page=5`);
+    // const res = await axios.get(`https://api.github.com/repos/mo-jo-dev/Github_Repo_Finder/languages`);
     // res.data.objects.forEach(i => {
         //     console.log(i);
         // });
